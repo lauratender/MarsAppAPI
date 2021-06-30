f22e02956a73a599f288e1b5e62ee8e193d98693
Laura Tender <lau.tender@gmail.com>
2021-06-30 15:11:24 +0300

    Added path params for camera and rover

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import express, { Express } from "express";
-import axios from 'axios';
-import {API_KEY} from "./info";
+import axios from "axios";
+import { API_KEY } from "./info";
  
 const app = express();
 const port = 8000;
@@ -22,20 +22,21 @@ enum Cameras {
   minites,
 }
 
-// 2
-const camera_type: string = Cameras[0];
-const rover_name: string = "Curiosity";
-
+// 4
 const router = express.Router();
 
-router.get('/rovers/photos', function (req, res) {
+router.get("/rovers/:rover/photos/:camera", function (req, res) {
+
+  //console.log(req.params);
+  const camera_type: string = req.params.camera;
+  const rover_name: string = req.params.rover;
 
   console.log(`Getting photos made with ${camera_type.toUpperCase()} from rover ${rover_name} ...`);
   const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover_name}/photos?sol=1000&camera=${camera_type}&api_key=${API_KEY}`;
 
   axios.get(URL)
   .then((response) => {
-    let result = response.data;
+    const result = response.data;
     res.send(result);
   })
   .catch(error => {
@@ -44,10 +45,11 @@ router.get('/rovers/photos', function (req, res) {
 
 });
 
-app.use('/', router);
+app.use("/", router);
 
 app.listen(port, () => {
-  console.log(`Test backend is running on port ${port}`);
+  console.log(`Backend is running on port ${port}`);
 });
 
-// 3 At "http://localhost:8000/rovers/photos" we can see the JSON with information about rovers' photos
+// 4 At "http://localhost:8000/rovers/rover_name/photos/camera_type" 
+// we can see the JSON with information about photos made with specificied rover and camera on sol 1000.
