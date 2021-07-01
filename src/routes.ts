84467ebc753499384db3674458268fd0a3ba6a13
Laura Tender <lau.tender@gmail.com>
2021-07-01 09:37:59 +0300

    Improved indentation, function name

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import { API_KEY } from "./info";
 import { checkCameraType } from "./cameras";
 import { reduceInfo } from "./photo";
-import { getHtml } from "./helper";
+import { getHtmlContentFromPhotos } from "./getHtmlContentFromPhotos";
 
 export const router = express.Router();
 
@@ -11,21 +11,20 @@ export const router = express.Router();
 
 router.get("/rovers", function (req, res) {
 
-    console.log("Getting rovers ...");
     const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers?api_key=${API_KEY}`;
 
     axios.get(URL)
-    .then((response) => {
-        const result = response.data;
-        res.send(result);
-    })
-    .catch(error => {
-        console.log(error)
-    });
-
+         .then((response) => {
+            const result = response.data;
+            res.send(result);
+         })
+         .catch(error => {
+            console.log(error) 
+         });
 });
 
 // Part 3 - 4
+
 router.get("/rovers/:rover/photos/:camera", function (req, res) {
 
     const roverName: string = req.params.rover;
@@ -33,19 +32,18 @@ router.get("/rovers/:rover/photos/:camera", function (req, res) {
   
     checkCameraType(cameraType);
 
-    console.log(`Getting photos made with ${cameraType.toUpperCase()} from rover ${roverName} ...`);
     const URL = `https://api.nasa.gov/mars-photos/api/v1/rovers/${roverName}/photos?sol=1000&camera=${cameraType}&api_key=${API_KEY}`;
 
     axios.get(URL)
-    .then((response) => {
-        const result = response.data;
-        const resPhotos = reduceInfo(result.photos);
-        
-        res.setHeader("Content-type", "text/html");
-        const resHtml: string = getHtml(resPhotos);
-        res.send(resHtml);
-    })
-    .catch(error => {
-        console.log(error)
-    });
+         .then((response) => {
+            const result = response.data;
+            const resPhotos = reduceInfo(result.photos);
+
+            res.setHeader("Content-type", "text/html");
+            const resHtml: string = getHtmlContentFromPhotos(resPhotos);
+            res.send(resHtml);
+         })
+         .catch(error => {
+            console.log(error)
+         });
 });
